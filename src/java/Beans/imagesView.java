9ffade48205d8855ac80e91dc04b2bb9959683f6
Alan Jafet González <alanjafetgm11@hotmail.com>
2017-06-19 01:21:25 -0500

    Interfaz al 100

---
+++ src/java/Beans/imagesView.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+/**
+ *
+ * @author alanj
+ */
+
+@ManagedBean
+@ViewScoped
+public class imagesView {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        images.add("imagen1.jpg");
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
